af6d46c12f0ba7dd30ce019366e38088c346df1c
Jordan Roher <jordan@notclickable.com>
2025-06-27 22:57:28 -0700

    Fixed Tailwind Version 0.2.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,ts,jsx,tsx}", "./templates/**/*.{html,js}"],
+	content: ["./templates/*"],
 	theme: {
 		extend: {},
 	},
